81ccfe959988e2641a475d5f5262f60b9dcba0f1
Salvatore Allegra <salallegra@gmail.com>
2020-03-22 00:23:12 -0700

    Initial commit

---
+++ src/test/java/com/salvatoreallegra/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salvatoreallegra.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
