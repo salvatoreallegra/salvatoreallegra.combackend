c69412a7c980b05240827864babd5d26db7a6f91
Salvatore Allegra <salallegra@gmail.com>
2020-04-10 16:32:40 -0700

    Deleted jsp views, will be using react for front and make rest api out of project

--- src/main/java/com/salvatoreallegra/controllers/LoginController.java
+++ src/main/java/com/salvatoreallegra/controllers/LoginController.java
@@ -1,6 +1,7 @@
 package com.salvatoreallegra.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,9 +11,8 @@ public class LoginController {
 	
 	
 	@RequestMapping("/login")
-	public String loginMessage(@RequestParam String name) {
-		
-		System.out.println(name);
+	public String loginMessage(@RequestParam String name, ModelMap model) {
+		model.put("name",name);
 		return "login";
 		
 	}
