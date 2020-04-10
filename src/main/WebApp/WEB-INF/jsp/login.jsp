3d8c1eaab6486991f2eeda262f216252e18f0a18
Salvatore Allegra <salallegra@gmail.com>
2020-04-10 14:24:52 -0700

    Add initial login.jsp view and LoginController

---
+++ src/main/WebApp/WEB-INF/jsp/login.jsp
@@ -0,0 +1,12 @@
+<html>
+<head>
+<title>
+	Login to salvatoreallegra.com
+</title>
+</head>
+<body>
+	<h1>Hi, I'm a jsp</h1>
+</body>
+
+
+</html>
--- src/main/java/com/salvatoreallegra/controllers/LoginController.java
+++ src/main/java/com/salvatoreallegra/controllers/LoginController.java
@@ -1,8 +1,19 @@
 package com.salvatoreallegra.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
 public class LoginController {
 	
-	public void DummyMethod() {
+	
+	@RequestMapping("/login")
+	public String loginMessage(@RequestParam String name) {
+		
+		System.out.println(name);
+		return "login";
 		
 	}
 
